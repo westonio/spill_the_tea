812404f9e1292ee7783b6a0709d4fbb3ca2a5e97
Weston Schutt <wtschutt@gmail.com>
2023-10-30 16:24:11 -0600

    Test: Add tests for Customer model

---
+++ spec/models/customer_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe Customer, type: :model do
+  describe 'Associations' do
+    # it { should have_many... belong_to :example }
+  end
+  
+  describe 'Validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :address }
+  end
+  
+  describe 'Instance Methods' do
+    let(:customer) { Customer.create!(name: 'Violet Chachki', email:'Violet.Chachki@gmail.com', address: '1234 Rainbow Road, Los Angeles, CA 90005') }
+    
+    it 'exists' do
+      expect(customer).to be_a(Customer)
+    end
+
+    it 'has a name, email, and address' do
+      expect(customer.name).to eq('Violet Chachki')
+      expect(customer.email).to eq('Violet.Chachki@gmail.com')
+      expect(customer.address).to eq('1234 Rainbow Road, Los Angeles, CA 90005')
+    end
+  end
+end
