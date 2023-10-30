b5838a795072081bd0708592c898913e1771526a
Weston Schutt <wtschutt@gmail.com>
2023-10-30 17:30:49 -0600

    Update: Add customer associations to subscriptions and teas

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,7 @@ class Customer < ApplicationRecord
   validates_presence_of :name, 
                         :email, 
                         :address
+
+  has_many :subscriptions
+  has_many :teas, through: :subscriptions
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
   describe 'Associations' do
-    # it { should have_many... belong_to :example }
+    it { should have_many :subscriptions }
+    it { should have_many :teas }
   end
   
   describe 'Validations' do
