cd8911edd586d1b721a30c1990bf6bd340b60cf0
Weston Schutt <wtschutt@gmail.com>
2023-10-30 17:34:54 -0600

    Test: Add validations, associations, and instance method tests for Subscriptions

---
+++ spec/models/subscription_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Subscription, type: :model do
+    let(:subscription) { Subscription.create!(title: 'Monthly Tea is Fundamental', price: 9.99, frequency: 1, status: 0) }
+  describe 'Associations' do
+     it { should have_one :tea }
+     it { should belong_to :customer }
+  end
+
+  describe 'Validations' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :price }
+    it { should validate_presence_of :status }
+    it { should validate_presence_of :frequency }
+
+    it { should validate_numericality_of :price }
+  end
+
+  describe 'Instance Methods' do
+    let(:customer) { create(:customer) }
+    let(:subscription) { Subscription.create!(title: 'Monthly Tea is Fundamental', price: 9.99, frequency: 1, status: 0, customer_id: customer.id) }
+
+    it 'exists' do
+      expect(subscription).to be_a(Subscription)
+      
+    end
+  end
+end
