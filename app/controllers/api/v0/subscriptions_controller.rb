7e6fcde89ca43073e46119c59c7f536d466c5fd8
Weston Schutt <wtschutt@gmail.com>
2023-10-31 14:24:27 -0600

    Feature: Update Subscription#create action to handle sad path errors

--- app/controllers/api/v0/subscriptions_controller.rb
+++ app/controllers/api/v0/subscriptions_controller.rb
@@ -1,6 +1,10 @@
 class Api::V0::SubscriptionsController < ApplicationController
   def create
-    render json: SubscriptionSerializer.new(Subscription.create!(subscription_params)), status: :created
+    begin
+      render json: SubscriptionSerializer.new(Subscription.create!(subscription_params)), status: :created
+    rescue StandardError => e
+      render json: ErrorSerializer.new(e).serialized_json, status: :unprocessable_entity
+    end
   end
   
   def index; end
