270d58ddf804b5ff5406d4e2ac1c2cb8b5be4fb3
Weston Schutt <wtschutt@gmail.com>
2023-10-31 14:24:01 -0600

    Test: Add tests for creating subscription Sad Paths

--- spec/requests/api/v0/subscriptions_spec.rb
+++ spec/requests/api/v0/subscriptions_spec.rb
@@ -2,11 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'Subscriptions Endpoints', type: :request do
   describe 'POST api/v0/subscriptions' do
-    let(:customer) { create(:customer) }
-    let(:tea) { create(:tea) }
-
     context 'When valid subscription parameters are used' do
       it 'sends 201 status code and creates tea_subscription object' do
+        customer = create(:customer) 
+        tea = create(:tea) 
         headers = { 'Content-Type' => 'application/json' }
         params = { 
                   customer_id: customer.id, 
@@ -44,6 +43,70 @@ RSpec.describe 'Subscriptions Endpoints', type: :request do
         expect(attributes[:frequency]).to be_a(String)
         expect(attributes[:tea_id]).to be_a(Integer)
         expect(attributes[:customer_id]).to be_a(Integer)
+      end
+    end
+
+    context 'When invalid customer or tea IDs are used' do
+      it 'Sends error message and unprocessable entity code' do
+        invalid_id = 999999999
+        tea = create(:tea)
+        headers = { 'content-type' => 'application/json' }
+        params = {
+          customer_id: invalid_id,
+          tea_id: tea.id,
+          title: "Quarterly tea is the bee's knees",
+          price: 24.99,
+          frequency: 2
+        }
+
+        post '/api/v0/subscriptions', headers: headers, params: JSON.generate(subscription: params)
+
+        expect(response).to_not be_successful
+        expect(response.status).to eq(422)
+
+        body = JSON.parse(response.body, symbolize_names: true)
+
+        expect(body).to be_a(Hash)
+        expect(body).to have_key(:errors)
+        expect(body[:errors]).to be_an(Array)
+
+        errors = body[:errors].first
+
+        expect(errors).to be_a(Hash)
+        expect(errors).to have_key(:details)
+        expect(errors[:details]).to eq('Validation failed: Customer must exist')
+      end
+    end
+
+      context 'When invalid attributes are used' do
+      it 'Sends error message and unprocessable entity code' do
+        customer = create(:customer)
+        tea = create(:tea)
+        headers = { 'content-type' => 'application/json' }
+        params = {
+          customer_id: customer.id,
+          tea_id: tea.id,
+          title: nil, # No title attribute
+          price: 24.99,
+          frequency: 2
+        }
+
+        post '/api/v0/subscriptions', headers: headers, params: JSON.generate(subscription: params)
+
+        expect(response).to_not be_successful
+        expect(response.status).to eq(422)
+
+        body = JSON.parse(response.body, symbolize_names: true)
+
+        expect(body).to be_a(Hash)
+        expect(body).to have_key(:errors)
+        expect(body[:errors]).to be_an(Array)
+
+        errors = body[:errors].first
+
+        expect(errors).to be_a(Hash)
+        expect(errors).to have_key(:details)
+        expect(errors[:details]).to eq("Validation failed: Title can't be blank")
       end
     end
   end
