3481e18f7c43f5cfb539636ff598ce46a9fe24b2
Weston Schutt <wtschutt@gmail.com>
2023-10-31 15:07:43 -0600

    Feature: Add subscriptions#update action with error handler

--- app/controllers/api/v0/subscriptions_controller.rb
+++ app/controllers/api/v0/subscriptions_controller.rb
@@ -1,4 +1,8 @@
 class Api::V0::SubscriptionsController < ApplicationController
+  def index; end
+
+  def show; end
+
   def create
     begin
       render json: SubscriptionSerializer.new(Subscription.create!(subscription_params)), status: :created
@@ -6,10 +10,15 @@ class Api::V0::SubscriptionsController < ApplicationController
       render json: ErrorSerializer.new(e).serialized_json, status: :unprocessable_entity
     end
   end
-  
-  def index; end
 
-  def show; end
+  def update
+    begin
+      render json: SubscriptionSerializer.new(Subscription.update!(subscription_params)), status: :ok
+    rescue StandardError => e
+      render json: ErrorSerializer.new(e).serialized_json, status: :bad_request
+    end
+  end
+  
 
   private
 
