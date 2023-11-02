2d479b039e11c4c0cd657e09ab350015e9d82bac
Weston Schutt <wtschutt@gmail.com>
2023-11-02 12:05:40 -0600

    Refactor: DRY up the rescuing of errors in sub controller

--- app/controllers/api/v0/subscriptions_controller.rb
+++ app/controllers/api/v0/subscriptions_controller.rb
@@ -1,31 +1,23 @@
 class Api::V0::SubscriptionsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+  rescue_from ArgumentError, with: :argument_invalid
+
   def index 
-    begin
-      customer = Customer.find(params[:customer_id])
-      render json: SubscriptionSerializer.new(customer.subscriptions), status: :ok
-    rescue StandardError => e
-      render json: ErrorSerializer.new(e).serialized_json, status: :not_found
-    end
+    customer = Customer.find(params[:customer_id])
+    render json: SubscriptionSerializer.new(customer.subscriptions), status: :ok
   end
 
   def show; end
 
   def create
-    begin
-      render json: SubscriptionSerializer.new(Subscription.create!(subscription_params)), status: :created
-    rescue StandardError => e
-      render json: ErrorSerializer.new(e).serialized_json, status: :unprocessable_entity
-    end
+    render json: SubscriptionSerializer.new(Subscription.create!(subscription_params)), status: :created
   end
 
   def update
-    begin
-      subscription = Subscription.find(params[:id])
-      subscription.update(subscription_params)
-      render json: SubscriptionSerializer.new(subscription), status: :ok
-    rescue StandardError => e
-      render json: ErrorSerializer.new(e).serialized_json, status: :bad_request
-    end
+    subscription = Subscription.find(params[:id])
+    subscription.update(subscription_params)
+    render json: SubscriptionSerializer.new(subscription), status: :ok
   end
   
 
@@ -33,5 +25,17 @@ class Api::V0::SubscriptionsController < ApplicationController
 
   def subscription_params
     params.require(:subscription).permit(:customer_id, :tea_id, :title, :price, :frequency, :status)
+  end
+
+  def record_not_found(error)
+    render json: ErrorSerializer.new(error).serialized_json, status: :not_found
+  end
+
+  def record_invalid(error)
+    render json: ErrorSerializer.new(error).serialized_json, status: :unprocessable_entity
+  end
+
+  def argument_invalid(error)
+    render json: ErrorSerializer.new(error).serialized_json, status: :bad_request
   end
 end
